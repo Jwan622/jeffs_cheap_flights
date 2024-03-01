50b08fc4501b3b575c1bbbdc7f3d23ff195bbb51
Jeffrey <jwan622@gmail.com>
2024-03-01 01:53:51 -0500

    fix dollar sign bug

--- nextjs_jeffs_cheap_flights_client/components/FullSearchDetails/FlightSearchDetails.tsx
+++ nextjs_jeffs_cheap_flights_client/components/FullSearchDetails/FlightSearchDetails.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './FlightSearchDetails.module.css';
+
 interface FlightSearchDetailsProps {
   flights: {
     lowest_price: string,
-    typical_price_range: string,
+    typical_price_range: string[],
     price_level: string,
   },
   className: string;
@@ -12,8 +13,8 @@ const FlightSearchDetails: React.FC<FlightSearchDetailsProps> = ({flights, class
   <div className={className}>
     <div className={styles.detailHeader}>Other Route Deets:</div>
     <div className={styles.detail}>Lowest Price: ${flights.lowest_price}</div>
-    <div className={styles.detail}>Typical Price Range: ${flights.typical_price_range}</div>
-    <div className={styles.detail}>Price Level: {flights.price_level}</div>
+    <div className={styles.detail}>Typical Price Range: ${flights.typical_price_range[0]} - ${flights.typical_price_range[1]}</div>
+    <div className={styles.detail}>Price Level: {flights.price_level[0].toUpperCase() + flights.price_level.slice(1)}</div>
   </div>
 )
 
