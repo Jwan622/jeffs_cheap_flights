754ff7813ca4324b9345dd7e5f07b3ba9da29ba6
Jeffrey <jwan622@gmail.com>
2024-03-05 23:53:02 -0500

    quick change in controller

--- jeffs_cheap_flights_api/app/services/flight_aggregator_service.rb
+++ jeffs_cheap_flights_api/app/services/flight_aggregator_service.rb
@@ -1,9 +1,7 @@
-
 class FlightAggregatorService
   def self.fetch()
-    data = SerpPresenter.new(SerpClient.fetch()).present
-
-    return data
+    SerpPresenter.new(SerpClient.fetch()).present
+    # A task to do: incorporate other apis and extend the front end to support the new data type
     # kayak_flights = KayakDecorator.new(Kayak.fetch()).decorate
     # return {google_flights: google_flights, kayak_flights: kayak_flights}
   end
